2223644b31a5a02a9db11ba89b10b4c2fad41feb
A.O.S <achyutha.os@vittindia.com>
2026-04-15 00:23:09 +0530

    added important change

---
+++ exercise_03/important.py
@@ -0,0 +1,8 @@
+print("""Python is a high-level, interpreted programming language 
+known for its readability and versatility. It supports multiple 
+programming paradigms, including structured, object-oriented, 
+and functional programming. Its large standard library and 
+active community make it one of the most popular languages 
+in the world today.""")
+
+# print("This is very important change.")
