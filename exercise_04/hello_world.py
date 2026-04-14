a6edcb433ce373dea9a9694328a06aa6deeb8fa6
A.O.S <achyutha.os@vittindia.com>
2026-04-15 00:33:03 +0530

    more changes

--- exercise_04/hello_world.py
+++ exercise_04/hello_world.py
@@ -1,9 +1,10 @@
+### This file is about printing out some information about Python and Java.
 print("""Python is a high-level, interpreted programming language 
 known for its readability and versatility. It supports multiple 
 programming paradigms, including structured, object-oriented, 
 and functional programming. Its large standard library and 
 active community make it one of the most popular languages 
-in the world today.""")
+in the world today. Additionally we use python for data science, machine learning, web development, automation, and many other applications.""")
 
 print("""Java is a high-level, class-based, object-oriented 
 programming language that is designed to have as few 
