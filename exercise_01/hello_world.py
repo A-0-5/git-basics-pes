7b35bc10378004a84ca758cda66d35f3f2c3e26d
Nagashree G <nagashreegopinath@gmail.com>
2026-04-15 16:15:23 +0530

    added HI

--- exercise_01/hello_world.py
+++ exercise_01/hello_world.py
@@ -3,4 +3,4 @@ known for its readability and versatility. It supports multiple
 programming paradigms, including structured, object-oriented, 
 and functional programming. Its large standard libary and 
 active community make it one of the most popular languages 
-in the world today.""")
+in the world today. HI""")
